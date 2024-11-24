f62ba5a0ef75c13103e1475278dd92d34754624c
manjeetdhayal <manjeetdhayal786@gmail.com>
2024-11-24 22:45:18 +0530

    Added userPrivacy

--- lib/SettingsPage.dart
+++ lib/SettingsPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_authentication/Firebase/Database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SettingsPage extends StatefulWidget {
   @override
@@ -13,29 +14,55 @@ class _SettingsPageState extends State<SettingsPage> {
   final TextEditingController _ageController = TextEditingController();
   final Database _database = Database();
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool isPrivate = false;
 
-  Future<void> _saveChanges() async {
-    if (_formKey.currentState?.validate() ?? false) {
+  @override
+  void initState() {
+    super.initState();
+    _loadPrivacySetting();
+  }
+
+  Future<void> _loadPrivacySetting() async {
+    String email = _auth.currentUser!.email!;
+    DocumentSnapshot doc = await FirebaseFirestore.instance.collection('Privacy').doc(email).get();
+    setState(() {
+      isPrivate = doc['isPrivate'] ?? false;
+    });
+  }
+
+  Future<void> _updatePrivacySetting(bool value) async {
+    String email = _auth.currentUser!.email!;
+    await FirebaseFirestore.instance.collection('Privacy').doc(email).set({'isPrivate': value});
+    setState(() {
+      isPrivate = value;
+    });
+  }
+
+  Future<void> _saveUsername() async {
+    if (_usernameController.text.isNotEmpty) {
       try {
         String email = _auth.currentUser!.email!;
-        Map<String, dynamic> updatedData = {};
-
-        if (_usernameController.text.isNotEmpty) {
-          updatedData['username'] = _usernameController.text;
-        }
-        if (_ageController.text.isNotEmpty) {
-          updatedData['age'] = _ageController.text;
-        }
-
-        if (updatedData.isNotEmpty) {
-          await _database.updateUserProfile(email, updatedData);
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Profile updated successfully')));
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('No changes to update')));
-        }
+        await _database.updateUserProfile(email, {'name': _usernameController.text});
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Username updated successfully')));
       } catch (e) {
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to update profile: $e')));
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to update username: $e')));
       }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Please enter your username')));
+    }
+  }
+
+  Future<void> _saveAge() async {
+    if (_ageController.text.isNotEmpty) {
+      try {
+        String email = _auth.currentUser!.email!;
+        await _database.updateUserProfile(email, {'age': _ageController.text});
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Age updated successfully')));
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Failed to update age: $e')));
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Please enter your age')));
     }
   }
 
@@ -51,31 +78,59 @@ class _SettingsPageState extends State<SettingsPage> {
           key: _formKey,
           child: Column(
             children: [
-              TextFormField(
-                controller: _usernameController,
-                decoration: InputDecoration(labelText: 'Username'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your username';
-                  }
-                  return null;
-                },
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 30.0),
+                    child: TextFormField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(labelText: 'Username'),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your username';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.save, color: Colors.green,),
+                    onPressed: _saveUsername,
+                  ),
+                ],
               ),
-              TextFormField(
-                controller: _ageController,
-                decoration: InputDecoration(labelText: 'Age'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your age';
-                  }
-                  return null;
-                },
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 30.0),
+                      child: TextFormField(
+                        controller: _ageController,
+                        decoration: InputDecoration(labelText: 'Age'),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your age';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.save, color: Colors.green,),
+                    onPressed: _saveAge,
+                  ),
+                ],
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: _saveChanges,
-                child: Text('Save Changes'),
+              SwitchListTile(
+                title: Text('Private Account'),
+                value: isPrivate,
+                onChanged: (bool value) {
+                  _updatePrivacySetting(value);
+                },
               ),
             ],
           ),
--- lib/UserProfilePage.dart
+++ lib/UserProfilePage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_authentication/Model/UserProfile.dart';
 
@@ -6,27 +7,53 @@ class UserProfilePage extends StatelessWidget {
 
   UserProfilePage({required this.userProfile});
 
+  Future<bool> _fetchPrivacySetting() async {
+    String email = userProfile.email;
+    DocumentSnapshot doc = await FirebaseFirestore.instance.collection('Privacy').doc(email).get();
+    return doc['isPrivate'] ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(userProfile.name),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Name: ${userProfile.name}', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 10),
-            Text('Email: ${userProfile.email}', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 10),
-            Text('Age: ${userProfile.age}', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 10),
-            Text('Hobby: ${userProfile.hobby}', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 10),
-            userProfile.imageUrl.isNotEmpty
-                ? Padding(
+    return FutureBuilder<bool>(
+      future: _fetchPrivacySetting(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(userProfile.name),
+            ),
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }  else {
+          bool isPrivate = snapshot.data ?? false;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(userProfile.name),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: isPrivate
+                  ? Container(
+                margin: EdgeInsets.all(20),
+                child: Text(
+                  "You don't have access to this user's profile.",
+                  style: TextStyle(fontSize: 20),
+                ),
+              )
+                  : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Name: ${userProfile.name}', style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 10),
+                  Text('Email: ${userProfile.email}', style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 10),
+                  Text('Age: ${userProfile.age}', style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 10),
+                  Text('Hobby: ${userProfile.hobby}', style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 10),
+                  userProfile.imageUrl.isNotEmpty
+                      ? Padding(
                     padding: const EdgeInsets.all(10.0),
                     child: Image.network(
                       userProfile.imageUrl,
@@ -35,14 +62,17 @@ class UserProfilePage extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   )
-                : Container(),
-            SizedBox(height: 10),
-            Text('Likes count: ${userProfile.like}', style: TextStyle(fontSize: 20)),
-            SizedBox(height: 10),
-            Text('Dislikes count: ${userProfile.dislike}', style: TextStyle(fontSize: 20)),
-          ],
-        ),
-      ),
+                      : Container(),
+                  SizedBox(height: 10),
+                  Text('Likes count: ${userProfile.like}', style: TextStyle(fontSize: 20)),
+                  SizedBox(height: 10),
+                  Text('Dislikes count: ${userProfile.dislike}', style: TextStyle(fontSize: 20)),
+                ],
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
